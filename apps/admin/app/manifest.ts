1de499d030dfc2066f63c221ab2759c9fc94b600
jylee <jylee@saycore.io>
2026-02-26 12:37:33 +0900

    fix: 관리자 페이지 manifest 파일 수정

--- apps/admin/app/manifest.ts
+++ apps/admin/app/manifest.ts
@@ -9,10 +9,31 @@ export default function manifest() {
     theme_color: '#fff',
     icons: [
       {
-        'src': '/images/logo.png',
-        'sizes': '192x192',
-        'type': 'image/png',
-        'purpose': 'any maskable',
+        src: '/images/logo.png',
+        sizes: '1024x1024',
+        type: 'image/png',
+        purpose: 'any',
+      },
+      {
+        src: '/images/logo.png',
+        sizes: '1024x1024',
+        type: 'image/png',
+        purpose: 'maskable',
+      },
+    ],
+    screenshots: [
+      {
+        src: '/images/pwa-install-wide.png',
+        sizes: '1280x720',
+        type: 'image/png',
+        form_factor: 'wide',
+        label: '관리자 대시보드 데스크톱 화면',
+      },
+      {
+        src: '/images/pwa-install-mobile.png',
+        sizes: '720x1280',
+        type: 'image/png',
+        label: '관리자 대시보드 모바일 화면',
       },
     ],
   };
